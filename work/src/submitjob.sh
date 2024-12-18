5663786a022c8922a549294610ff7f1120370ec9
DeepWater3104 <fukami.satoshi760@mail.kyutech.jp>
2024-12-18 12:18:07 +0900

    update

--- work/src/submitjob.sh
+++ work/src/submitjob.sh
@@ -32,7 +32,7 @@ cat << EOF > job.sh
 
 #PJM --rsc-list "node=1"
 #PJM -g hp200139
-#PJM --rsc-list "elapse=00:10:00"
+#PJM --rsc-list "elapse=01:30:00"
 #PJM -m b,e
 #PJM --mail-list "fukami.satoshi760@mail.kyutech.jp"
 #PJM -j
@@ -44,7 +44,18 @@ cat << EOF > job.sh
 
 module load lang
 export PLE_MPI_STD_EMPTYFILE=off
-fapp -C -d ../detailed_profile/ -Icpupa,cputime,nompi -Hevent=statistics ./run 10 10
+
+fapp -C -d ../detailed_profile/rep1  -Hevent=pa1 ./run 10 10
+fapp -C -d ../detailed_profile/rep2  -Hevent=pa2 ./run 10 10
+fapp -C -d ../detailed_profile/rep3  -Hevent=pa3 ./run 10 10
+fapp -C -d ../detailed_profile/rep4  -Hevent=pa4 ./run 10 10
+fapp -C -d ../detailed_profile/rep5  -Hevent=pa5 ./run 10 10
+fapp -C -d ../detailed_profile/rep6  -Hevent=pa6 ./run 10 10
+fapp -C -d ../detailed_profile/rep7  -Hevent=pa7 ./run 10 10
+fapp -C -d ../detailed_profile/rep8  -Hevent=pa8 ./run 10 10
+fapp -C -d ../detailed_profile/rep9  -Hevent=pa9 ./run 10 10
+fapp -C -d ../detailed_profile/rep10  -Hevent=pa10 ./run 10 10
+fapp -C -d ../detailed_profile/rep11  -Hevent=pa11 ./run 10 10
 EOF
 
 
